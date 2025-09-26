8a9dfa4a35580909fdb44372b1e5d996612927e9
Kavz44 <kavingregary@outlook.com>
2025-09-27 00:00:49 +0800

    Initial Commit

---
+++ MySecondApp/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  MySecondApp
+//
+//  Created by Kavin Gregary Anand on 27/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        VStack {
+            Button("Button") {
+                
+            }
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySecondApp/MySecondAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySecondAppApp.swift
+//  MySecondApp
+//
+//  Created by Kavin Gregary Anand on 27/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MySecondAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
