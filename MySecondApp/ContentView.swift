f866019f5bc774bb9bdba4a05cec6f87ac7580e9
Kavz44 <kavingregary@outlook.com>
2025-09-27 01:27:33 +0800

    clean before next lesson

--- MySecondApp/ContentView.swift
+++ MySecondApp/ContentView.swift
@@ -9,57 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Color(.systemMint)
-                .ignoresSafeArea()
-            
-            VStack(alignment: .leading, spacing: 20.0){
-                Image("martinO")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(15)
-                
-                HStack{
-                    
-                    Text("Mr. Ødda")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    Image(systemName: "soccerball")
-                    Spacer()
-                    
-                    VStack{
-                        HStack{
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                        }
-                        
-                        Text("5 stars!")
-                    }
-                    .foregroundColor(.orange)
-                    
-                }
-                
-                Text("\"I want to do everything perfectly. I want to be the best, all the time\"")
-            }
-            .padding()
-            .background(Rectangle().foregroundStyle(.white)
-                .cornerRadius(15)
-                .shadow(radius: 15))
-            .padding()
-            
-            
-            
-        }
-        }
         
         
+        Text("test")
         
-    
+        
+    }
 }
-
 #Preview {
     ContentView()
 }
