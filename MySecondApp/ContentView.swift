8537797ec0b026becf49b4f36872baa06764472d
Kavz44 <kavingregary@outlook.com>
2025-09-27 01:50:40 +0800

    made the initial design og the game UI

--- MySecondApp/ContentView.swift
+++ MySecondApp/ContentView.swift
@@ -10,8 +10,73 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
+        ZStack {
+            // Bakgrunn
+            Image("background-plain")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            
+            VStack(spacing: 40) {
+                Spacer()
+                
+                // War Logo
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                
+                Spacer()
+                
+                // Kortene
+                HStack(spacing: 30) {
+                    Image("card2")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 120, height: 180)
+                    
+                    Image("card10")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 120, height: 180)
+                }
+                
+                Spacer()
+                
+                // Deal knappen
+                Image("button")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 60)
+                
+                //Spacer()
+                
+                // Scoreboarden
+                HStack(spacing: 50) {
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                        Text("0")
+                            .font(.largeTitle)
+                            .bold()
+                    }
+                    
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                        Text("0")
+                            .font(.largeTitle)
+                            .bold()
+                    }
+                }
+                .foregroundColor(.white)
+                
+                Spacer()
+            }
+            .padding(.horizontal, 30)
+        }
+
         
-        Text("test")
         
         
     }
